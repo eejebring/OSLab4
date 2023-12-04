429d320f2853b80dd566703afd6ddbbe9bb2b340
eejebring <dev@ejebring.com>
2023-12-04 19:12:07 +0100

    blink lights

--- src/lightControl.ts
+++ src/lightControl.ts
@@ -5,6 +5,7 @@ const Gpio = onoff.Gpio
 export class LightControl {
 	//private light: onoff.Gpio
 	private state: boolean = false
+	private Intervall: NodeJS.Timeout | undefined
 	private pin: number
 
 	constructor(pin: number) {
@@ -17,12 +18,27 @@ export class LightControl {
 	}
 
 	public setState(state: boolean) {
+		this.stopBlink()
 		this.state = state
 		console.log("light: ", this.pin, " turned " + (this.state ? "on!" : "off!"))
 	}
 
 	public toggle() {
 		this.state = !this.state
+		console.log("light: ", this.pin, " toggled " + (this.state ? "on!" : "off!"))
 		//this.light.writeSync(this.state ? 1 : 0)
+	}
+
+	public blink(intervalTime: number) {
+		this.stopBlink()
+		this.Intervall = setInterval(() => {
+			this.toggle()
+		}, intervalTime)
+	}
+
+	public stopBlink() {
+		if (this.Intervall) {
+			clearInterval(this.Intervall)
+		}
 	}
 }
